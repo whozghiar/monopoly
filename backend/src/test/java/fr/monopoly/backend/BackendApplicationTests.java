c70cd06aa46b44277e0e3b778ea24509bee48f9e
Theo Lomege <theolomege@gmail.com>
2024-05-11 12:39:16 +0200

    Configuration du front-end - Création du POM avec les dépendances initiales (Spring,JPA & MariaDB) - Créatio nd'un dossier infra avec un docker-compose contenant la base de données

---
+++ backend/src/test/java/fr/monopoly/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.monopoly.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
